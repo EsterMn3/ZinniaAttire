0941bb5f9e2978b272e94d1b2d7302c031548546
EsterMn3 <esterminja86@gmail.com>
2024-09-29 00:12:21 +0200

    Fix header responsiveness

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,6 +1,6 @@
 export default function Shop({ children }) {
   return (
-    <section id="shop">
+    <section id="shop" className="container">
       <h2>Wear Your Confidence</h2>
 
       <ul id="products">{children}</ul>
